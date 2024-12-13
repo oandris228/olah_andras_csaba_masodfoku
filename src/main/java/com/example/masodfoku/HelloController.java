93bd7bab3a95a7934f93c724cca36fecf63693ce
Oláh András <106300817+oandris228@users.noreply.github.com>
2024-12-13 16:40:55 +0100

    init + not done

---
+++ src/main/java/com/example/masodfoku/HelloController.java
@@ -0,0 +1,57 @@
+package com.example.masodfoku;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class HelloController {
+
+    @FXML
+    private TextField num1;
+    @FXML
+    private TextField num3;
+    @FXML
+    private Label result2;
+    @FXML
+    private TextField num2;
+    @FXML
+    private Label result1;
+
+
+    @FXML
+    public void solveEquation(ActionEvent actionEvent) {
+        try {
+            System.out.println("hehe");
+            if (!num1.getText().equals("") || !num3.getText().equals("") || !num2.getText().equals("")) {
+                double a = Double.parseDouble(num1.getText());
+                double b = Double.parseDouble(num2.getText());
+                double c = Double.parseDouble(num3.getText());
+
+                double sqrttemp = Math.sqrt(Math.pow(b, 2)-4*a*c);
+                System.out.println("hehe2");
+
+                if (sqrttemp < 0) {
+                    result1.setText("Sqrt Error");
+                } else {
+                    System.out.println("hehe3");
+                    double d = (-a + sqrttemp)/2*a;
+                    double e = (-a - Math.sqrt(Math.pow(b, 2)-4*a*c))/2*a;
+
+                    if (d == e) {
+                        result1.setText("x = " + d);
+                        result2.setText("");
+                    } else {
+                        System.out.println("hehe4");
+                        result1.setText("x1 = " + d);
+                        result2.setText("x2 = " + e);
+
+                    }
+                }
+            }
+        } catch (NumberFormatException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
